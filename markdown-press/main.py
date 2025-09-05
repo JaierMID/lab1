9eb5411111bba8dfd46d794962c7cb65ce2d7760
hewu888 <HewuBa00@gmail.com>
2025-09-05 17:42:38 +0800

    test

---
+++ markdown-press/main.py
@@ -0,0 +1,7 @@
+from generator import generate_site
+
+if __name__ == "__main__":
+    print("--- Starting MarkdownPress Generator ---")
+    generate_site()
+    print("--- Finished. Check the 'public' folder. ---")
+    
---
+++ markdown-press/source/index.md
@@ -0,0 +1,5 @@
+# Welcome to My Website!
+This is the homepage. It's generated from a simple Markdown file.
+- Item 1
+- Item 2
+- 
